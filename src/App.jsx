848e53a186a06cbd634ed82c792fb856316259a8
Kwabena Junior <samuelassanjnr@gmail.com>
2026-01-14 18:36:47 +0000

    Initial commit - portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+// App.jsx
+function App() {
+
+   
+  return (
+    <Header /> 
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import scrollbar from "tailwind-scrollbar";
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#6366f1",
+        dark: "#0f172a",
+      },
+      fontFamily: {
+        heading: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [scrollbar],
+};
